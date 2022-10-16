6fd4c1419504f2c9c37f0fbd4c68f5d6a3a2939f
Eric White <led.ecodyswhite@gmail.com>
2022-10-16 04:39:44 -0700

    added requested items to lists

--- random_fursona.py
+++ random_fursona.py
@@ -10,7 +10,7 @@ add small weighted chance for special classes, like species fusions, galactic co
 """
 
 species_list = ["bird", "dog", "bunny", "cat", "avali", "hare", "protogen", "moth", "salamander", "chimera", "bat", "cow", "wolf", "fox", "coyote", "sheep", "goat", "deer", "dragon", "wyvern", "spider", "horse", "lion", "tiger", "bear", "shark",
-                "hyena", "bee", "lamia", "kangaroo", "possum", "wombat", "skunk", "slug", "sea slug", "moogle", "ferret", "weasel", "stoat", "otter", "dryad", "raccoon", "alligator", "crocodile", "snake", "rat", "mouse", "chipmunk", "squirrel", "fennec", "kitsune", "puma", "little pee pee boy", "pangolin", "mamagen"]
+                "hyena", "bee", "lamia", "kangaroo", "possum", "wombat", "skunk", "slug", "sea slug", "moogle", "ferret", "weasel", "stoat", "otter", "dryad", "raccoon", "alligator", "crocodile", "snake", "rat", "mouse", "chipmunk", "squirrel", "fennec", "kitsune", "puma", "pangolin", "mamagen"]
 color_list = ["aqua", "aquamarine", "azure", "beige", "bisque", "black", "blue", "blue violet", "brown", "chartreuse", "chocolate", "coral", "crimson", "cyan", "dark blue", "dark cyan", "dark goldenrod", "dark green", "dark khaki", "dark magenta", "dark olive green", "dark orange", "dark orchid", "dark red", "dark sea green", "dark turquoise", "dark violet", "deep pink", "deep sky blue", "dim gray", "floral white",
               "forest green", "fuchsia", "gold", "goldenrod", "gray", "green yellow", "hot pink", "indigo", "ivory", "khaki", "lavendar", "light blue", "light green", "light sea green", "lime", "magenta", "maroon", "midnight blue", "orange", "orange red", "orchid", "pink", "plum", "powder blue", "purple", "red", "sea green", "sienna", "silver", "sky blue", "teal", "turoquoise", "violet", "white", "yellow", "yellow green", "platinum", "fibromelanotic"]
 size_list = ["thimble-sized", "cup full of", "small", "medium", "large", "micro",
@@ -22,7 +22,7 @@ weakness_list = ["an aversion to blenders", "a fear of tea", "extreme clumsiness
 favorite_food = ["choconana", "pilk", "crayons", "pancakes", "your friends", "your enemies", "fuit gummy",
                  "chocolate covered espresso beans", "hotdog bong water", "nicotine gum", "corn husks", "paper", "spaghetti code", "war crime burger", "cake", "pound cake", "coffee cake", "donuts", "taco bell", "paint chips", "bepis", "estrogen", "wood", "testosterone", "cement", "dirt", "smart phones", "sushi", "whiny bottoms", "pocket lint", "guitar picks", "rare pokemon cards", "waffles", "souls", "mangos", "chocolate", "white chocolate", "chocolate bloom", "snozzberries", "weed", "rock candy", "edible clothing", "pastry creatures", "ice cubes", "focus crystal", "tiddy", "pennies", "lofi beats", "spirals", "algae", "polyfill", "the blood of your enemies"]
 role_list = ["healer", "tank", "mage", "bard", "dps", "support", "alchemist", "knight",
-             "deadbeat", "merchant", "diplomat", "rogue", "druid", "necromancer", "nekomancer", "geomancer", "seer", "hardass", "emergency food supply", "freeloader", "milk drinker", "imposter", "crewmate", "idol", "mime", "chef"]
+             "deadbeat", "merchant", "diplomat", "rogue", "druid", "necromancer", "nekomancer", "geomancer", "seer", "hardass", "emergency food supply", "freeloader", "milk drinker", "imposter", "crewmate", "idol", "mime", "chef" "little pee pee boy", "beef editor", "biochemist"]
 
 
 def rand(list):
