50f21c3f3a81d31184e044d308e1b622db074e21
Eric White <led.ecodyswhite@gmail.com>
2023-04-06 15:44:32 -0700

    printing unonboarded users works

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import text_coloring as tc
 import random_fursona, tarot, d20, chat_curses, snowstamp
 import datetime
 from datetime import datetime, timedelta
+import threading
 intents = discord.Intents.all()
 client = discord.Client(intents = intents)
 
@@ -10,26 +11,41 @@ client = discord.Client(intents = intents)
 @client.event
 async def on_ready():
   print("we have logged in as {0.user}".format(client))
-
-  counter = 0
+  then = datetime.now() - timedelta(days = 7)
   mem_list = list()
-  role = "gay gay homosexual gay"
+  role = "gay gay homosexual gay" # name of the default role of all users that complete onboarding
   purge_list = list()
   for member in client.get_all_members():
     mem_list.append({"id": member.id, "name": member.name, "messages_sent": 0, "roles": member.roles, "join_date": member.joined_at})
   for value in mem_list:
     if role in str(value["roles"]):
-      print(str(value["name"]))
-      # okay so that prints the role only when the user has the role.
-      # now we need to check if their join date is 7 days prior
-      # and if so, purge them.
-      # then loop the part of the on_ready function that checks that.
+      print(f'{str(value["name"])} has completed onboarding')
 
   # print(mem_list) #that'll give you a lot of members with a lot of information
-  #since you know that works, you don't need to print it every time now.
-
+  for value in mem_list:
+      if role not in (str(value["roles"])) and member.joined_at.timestamp() < then.timestamp():
+        purge_list.append(value)
+        print(f'{str(value["name"])} has not completed onboarding after one week')
+  print(purge_list)
     # push the member id to a table using sqlalchemy
     # the table should have member id, member name, and number of messages sent within 144 hours  
+
+  def checkTime():
+  # This function runs periodically every 1 second
+    threading.Timer(1, checkTime).start()
+
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    # print("Current Time =", current_time)
+    # comment that out to not pollute the console
+
+    if(current_time == '23:39:30'):  # check if matches with the desired time
+        print('sending message')
+        # make a function and have it called here
+
+
+  checkTime()
 
 @client.event
 async def on_message(message):
@@ -66,6 +82,7 @@ async def on_message(message):
   else:
     m_col = tc.new(member_col.r, member_col.g, member_col.b)
   c_col = tc.new(255,82,197) if not message.channel.nsfw else tc.R
+  # print(message.guild.id)
   print(message.author.name)
   print(f'in {c_col}{message.channel.name}, {m_col}{message.author.name}{tc.W}:')
   print(message.content)
@@ -89,7 +106,10 @@ async def on_message(message):
   # print(counter)
   # print(idList) # hiding this for now because tired of long list printing in terminal
   if counter >= 10:
-    print("congratulations, you're granted a priveleged role!")
+    # print("congratulations, you're granted a priveleged role!")
+    role = msg.guild.get_role(1079214033036660797) #need to get the specific role for specific server
+    #okay this works, now you need to have a time function to remove the role if less than 10 messages.
+    await msg.author.add_roles(role, reason=None, atomic=True)
     # after this print statement, you can append priveleged role status to sql database
     # via sqlalchemy
     # then, every so often, check the sql database to change roles
