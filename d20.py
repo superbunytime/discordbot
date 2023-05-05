8cb78bd78963e59b1ac746f09a5a0eab00bf3d4c
Eric White <led.ecodyswhite@gmail.com>
2023-05-05 09:36:10 -0700

    got any sided dice working; still dunno about db.

--- d20.py
+++ d20.py
@@ -1,13 +1,15 @@
 import random
 
 
-def d20():
-  num = (random.randrange(1,21))
-  if num == 1:
-    return "1... oof."
-  if num == 20:
-    return "20!  Critical!!"
-  else:
-    return num
+def d(n):
+  num = (random.randrange(1, n + 1))
+  if n == 20:
+    if num == 1:
+      return "1... oof."
+    if num == 20:
+      return "20!  Critical!!"
+    else:
+      return num
+  return num
 
-#print(d20())
+# print(d(621))
