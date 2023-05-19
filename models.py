45c24d08909ec3dad6f9070ac8094be4a8ecf21c
Eric White <led.ecodyswhite@gmail.com>
2023-05-19 08:11:48 -0700

    database is running without breaking the bot

--- models.py
+++ models.py
@@ -1,25 +1,16 @@
 import sqlalchemy
-from sqlalchemy import BigInteger, Column, Integer
+from sqlalchemy import BigInteger, Column, Integer, create_engine, String, Integer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Mapped, mapped_column
 
-db = sqlalchemy()
 
-engine = db.create_engine()
 
-class USER(db.model):
+class Base(DeclarativeBase):
+    pass
+class USER(Base):
     __tablename__ = 'users'
-    id = db.Column(db.BigInteger, primary_key = True)
-    name = db.Column(db.String, nullable = False)
-    active_user = db.Column(db.Boolean, nullable = False)
-    has_roles = db.Column(db.Boolean, nullable = False)
-    join_date = db.Column(db.DateTime, nullable = False)
-
-def connect_db(app):
-    """Call this in your bot on startup to connect.
-    This is code snagged from the flask app solution,
-    so it may need to be messed with to connect to the
-    discord app properly."""
-    
-    db.app = app
-    db.init_app(app)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    active_user: Mapped[str] = mapped_column(String(5))
+    has_roles: Mapped[str] = mapped_column(String(5))
+    join_date: Mapped[int] = mapped_column(Integer)
---
+++ queries.py
@@ -0,0 +1,7 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+import db, models
+
+print(db.engine)
+
+""""""
